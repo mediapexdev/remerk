ed1aab515e7c8c7b999c0e7d896b61f826c0509c
ziz9219 <aziz922010@gmail.com>
2022-12-12 21:08:10 +0000

    filtre des expéditions pour les transpoteurs effectué

--- resources/views/transporteur/components/expeditions/overview/overview.blade.php
+++ resources/views/transporteur/components/expeditions/overview/overview.blade.php
@@ -2,12 +2,21 @@
     use App\Models\Expedition;
     use App\Models\Expediteur;
     use App\Models\EtatExpedition;
+    use Illuminate\Support\Collection;
     
     $transporteur = isset($transporteur) ? $transporteur :
         Transporteur::where('user_id', Auth::user()->id)->first();
+    $vehicles = $transporteur->vehicules;
 
-    $available_expeditions = Expedition::where('etat_expedition_id', EtatExpedition::EN_ATTENTE)->orderByDesc('created_at')->limit(5)->get();
+    $available_expeditions = Collection::make();
+    $all_expeditions_available = Expedition::where('etat_expedition_id', EtatExpedition::EN_ATTENTE)->orderByDesc('created_at')->limit(5)->get();
 
+    foreach ($all_expeditions_available as $expedition) {
+        if($vehicles->contains(function ($vehicle) use($expedition) {
+            return $expedition->expeditionMatiere->types_vehicule_id == $vehicle->id; })){
+            $available_expeditions->push($expedition);
+        }
+    }
     $current_expeditions = Expedition::where('transporteur_id', $transporteur->id)->whereIn('etat_expedition_id', [
         EtatExpedition::EN_ATTENTE_DE_PAIEMENT,
         EtatExpedition::EN_ATTENTE_DE_CHARGEMENT,
