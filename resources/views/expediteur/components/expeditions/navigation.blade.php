57d0334af7066e9b0588475b4a6c0b0e5c7a928c
ziz9219 <aziz922010@gmail.com>
2022-12-14 14:09:05 +0000

    Test Errors

--- resources/views/expediteur/components/expeditions/navigation.blade.php
+++ resources/views/expediteur/components/expeditions/navigation.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="d-flex flex-wrap flex-stack gap-5 gap-lg-10">
     {{-- <!--begin::Menu--> --}}
     <ul id="expeditions_nav" class="nav nav-stretch nav-line-tabs nav-line-tabs-2x border-0 fs-5 fw-semibold mb-4 mb-xl-10 me-auto">
@@ -34,28 +33,3 @@
     </div>
     {{-- <!--end::Button Wrapper--> --}}
 </div>
-=======
-<ul id="expeditions_nav" class="nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-4 fw-semibold mb-8 mb-xl-10">
-    {{-- <!--begin::Item--> --}}
-    <li class="nav-item mt-2">
-        {{-- <!--begin::Link--> --}}
-        <a class="nav-link ms-0 me-10 py-5 active" href="#kt_tab_pending_expeditions" data-bs-toggle="tab">En attente</a>
-        {{-- <!--end::Link--> --}}
-    </li>
-    {{-- <!--end::Item--> --}}
-    {{-- <!--begin::Item--> --}}
-    <li class="nav-item mt-2">
-        {{-- <!--begin::Link--> --}}
-        <a class="nav-link ms-0 me-10 py-5" href="#kt_tab_expeditions_in_progress" data-bs-toggle="tab">En cours</a>
-        {{-- <!--end::Link--> --}}
-    </li>
-    {{-- <!--end::Item--> --}}
-    {{-- <!--begin::Item--> --}}
-    <li class="nav-item mt-2">
-        {{-- <!--begin::Link--> --}}
-        <a class="nav-link ms-0 me-10 py-5" href="#kt_tab_expeditions_made" data-bs-toggle="tab">Effectuées</a>
-        {{-- <!--end::Link--> --}}
-    </li>
-    {{-- <!--end::Item--> --}}
-</ul>
->>>>>>> 9781208d17d667b044e51dede422d2b9fadc4002
