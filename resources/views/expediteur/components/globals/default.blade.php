3551e060527c64a8c094c2c2c9055daf13caa570
Djielani Bodian <djielanibodian99@gmail.com>
2022-12-23 13:16:15 +0000

    resizes img

--- resources/views/expediteur/components/globals/default.blade.php
+++ resources/views/expediteur/components/globals/default.blade.php
@@ -2,34 +2,59 @@
     <!--begin::Body-->
     <div class="card-body d-flex flex-column justify-content-between mt-3 bgi-no-repeat bgi-size-cover bgi-position-x-center pb-0"
         style="background-position: 100% 50%; background-image:url('assets/media/stock/900x600/42.png')">
-        <!--begin::Wrapper-->
-        <div class="mb-3">
-            <!--begin::Title-->
-            <div class="fs-2hx fw-bold text-gray-800 text-center mb-7">
-                <span class="me-2">Try our all new Enviroment with
-                    <br />
-                    <span class="position-relative d-inline-block text-danger">
-                        <a href="">Pro Plan</a>
-                        <!--begin::Separator-->
-                        <span class="position-absolute opacity-15 bottom-0 start-0 border-4 border-danger border-bottom w-100"></span>
-                        <!--end::Separator-->
-                    </span></span>for Free
+        <div class="tns tns-default" style="direction: ltr">
+            <!--begin::Slider-->
+            <div data-tns="true" data-tns-loop="true" data-tns-swipe-angle="false" data-tns-speed="2000"
+                data-tns-autoplay="true" data-tns-autoplay-timeout="18000" data-tns-controls="true" data-tns-nav="false"
+                data-tns-items="1" data-tns-center="false" data-tns-dots="false"
+                data-tns-prev-button="#kt_team_slider_prev1" data-tns-next-button="#kt_team_slider_next1">
+
+                <!--begin::Item-->
+                <div class="text-center">
+                    <img src="assets/images/2D Pics/resizes/vehicle2-07-01.png" class="card-rounded img-fluid" alt="" />
+                    <button>test</button>
+                </div>
+                <!--end::Item-->
+                <!--begin::Item-->
+                <div class="text-center">
+                    <img src="assets/images/2D Pics/resizes/vehicle2-08-05.png" class="card-rounded img-fluid" alt="" />
+                </div>
+                <!--end::Item-->
+                <!--begin::Item-->
+                <div class="text-center">
+                    <img src="assets/images/2D Pics/resizes/vehicle2-07-02.png" class="card-rounded img-fluid" alt="" />
+                </div>
+                <!--end::Item-->
+                <!--begin::Item-->
+                <div class="text-center">
+                    <img src="assets/images/2D Pics/resizes/vehicle2-08-02.png" class="card-rounded img-fluid" alt="" />
+                </div>
+                <!--end::Item-->
+                <!--begin::Item-->
+                <div class="text-center">
+                    <img src="assets/images/2D Pics/resizes/vehicle2-07-03.png" class="card-rounded img-fluid" alt="" />
+                </div>
+                <!--end::Item-->
+                ...
             </div>
-            <!--end::Title-->
-            <!--begin::Action-->
-            <div class="text-center">
-                <a class="menu-link btn btn-sm btn-primary" data-bs-toggle="modal"
-                    data-bs-target="#kt_modal_create_expedition">
-                    <span class="menu-title text-white">Faire une expedition</span>
-                </a>
-            </div>
-            <!--begin::Action-->
+            <!--end::Slider-->
+
+            <!--begin::Slider button-->
+            <button class="btn btn-icon btn-active-color-primary" id="kt_team_slider_prev1">
+                <span class="svg-icon svg-icon-3x">
+                    ...
+                </span>
+            </button>
+            <!--end::Slider button-->
+
+            <!--begin::Slider button-->
+            <button class="btn btn-icon btn-active-color-primary" id="kt_team_slider_next1">
+                <span class="svg-icon svg-icon-3x">
+                    ...
+                </span>
+            </button>
+            <!--end::Slider button-->
         </div>
-        <!--begin::Wrapper-->
-        <!--begin::Illustration-->
-        <img class="mx-auto h-200px h-lg-250px theme-light-show" src="{{URL::asset('assets/images/Fitting_piece.gif')}}">
-        <img class="mx-auto h-200px h-lg-250px theme-dark-show" src="{{URL::asset('assets/images/Fitting_piece-dark.gif')}}">
-        <!--end::Illustration-->
     </div>
     <!--end::Body-->
 </div>
