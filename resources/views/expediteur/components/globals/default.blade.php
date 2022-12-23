3523b20457f393230beddbb87b6f36a6d7d940af
Djielani Bodian <djielanibodian99@gmail.com>
2022-12-23 15:34:03 +0000

    //**//

--- resources/views/expediteur/components/globals/default.blade.php
+++ resources/views/expediteur/components/globals/default.blade.php
@@ -1,51 +1,71 @@
 <div class="card card-flush h-100">
     <!--begin::Body-->
-    <div class="card-body d-flex flex-column justify-content-between mt-3 bgi-no-repeat bgi-size-cover bgi-position-x-center pb-0"
+    <div class="card-body d-flex flex-column justify-content-between bgi-no-repeat bgi-size-cover bgi-position-x-center p-0 px-0 pb-0"
         style="background-position: 100% 50%; background-image:url('assets/media/stock/900x600/42.png')">
         <div class="tns tns-default" style="direction: ltr">
             <!--begin::Slider-->
-            <div data-tns="true" data-tns-loop="true" data-tns-swipe-angle="false" data-tns-speed="2000"
+            <div data-tns="true" data-tns-loop="true" data-tns-swipe-angle="false" data-tns-speed="1000"
                 data-tns-autoplay="true" data-tns-autoplay-timeout="18000" data-tns-controls="true" data-tns-nav="false"
                 data-tns-items="1" data-tns-center="false" data-tns-dots="false"
                 data-tns-prev-button="#kt_team_slider_prev1" data-tns-next-button="#kt_team_slider_next1">
 
                 <!--begin::Item-->
-                <div class="text-center d-grid">
-                    <img src="assets/images/2D Pics/resizes/vehicle2-07-01.png" class="card-rounded h-50  img-fluid" alt="" />
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est sed sequi, corrupti rerum ad praesentium quam fuga placeat, corporis mollitia sint incidunt libero iste officiis voluptas totam! Sint, modi voluptatibus?
+                <div class="text-center">
+                    <img src="assets/images/2D Pics/resizes/vehicle2-07-01-resized.png" class=" img-fluid"
+                        alt="" />
+                    <p class="m-5">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est sed sequi, corrupti rerum ad
+                    </p>
+                    <button class="btn btn-primary">Action</button>
+                </div>
+                <!--end::Item-->
+                <!--begin::Item-->
+                <div class="text-center">
+                    <img src="assets/images/2D Pics/resizes/vehicle2-08-05.png" class="img-fluid"
+                        alt="" />
+                        <p class="m-5">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est sed sequi, corrupti rerum ad
                         </p>
-                    <button class="btn btn-primary d-block">Faire une expédition</button>
+                        <button class="btn btn-primary">Action</button>
                 </div>
                 <!--end::Item-->
                 <!--begin::Item-->
                 <div class="text-center">
-                    <img src="assets/images/2D Pics/resizes/vehicle2-08-05.png" class="card-rounded h-50 img-fluid" alt="" />
+                    <img src="assets/images/2D Pics/resizes/vehicle2-07-02-resized.png" class="img-fluid"
+                        alt="" />
+                        <p class="m-5">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est sed sequi, corrupti rerum ad
+                        </p>
+                        <button class="btn btn-primary">Action</button>
                 </div>
                 <!--end::Item-->
                 <!--begin::Item-->
                 <div class="text-center">
-                    <img src="assets/images/2D Pics/resizes/vehicle2-07-02.png" class="card-rounded h-50 img-fluid" alt="" />
+                    <img src="assets/images/2D Pics/resizes/vehicle2-08-02-resized.png" class="img-fluid"
+                        alt="" />
+                        <p class="m-5">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est sed sequi, corrupti rerum ad
+                        </p>
+                        <button class="btn btn-primary">Action</button>
                 </div>
                 <!--end::Item-->
                 <!--begin::Item-->
                 <div class="text-center">
-                    <img src="assets/images/2D Pics/resizes/vehicle2-08-02.png" class="card-rounded h-50 img-fluid" alt="" />
+                    <img src="assets/images/2D Pics/resizes/vehicle2-07-03-resized.png" class="img-fluid"
+                        alt="" />
+                        <p class="m-5">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est sed sequi, corrupti rerum ad
+                        </p>
+                        <button class="btn btn-primary">Action</button>
                 </div>
                 <!--end::Item-->
-                <!--begin::Item-->
-                <div class="text-center">
-                    <img src="assets/images/2D Pics/resizes/vehicle2-07-03.png" class="card-rounded h-50 img-fluid" alt="" />
-                </div>
-                <!--end::Item-->
-                ...
             </div>
             <!--end::Slider-->
 
             <!--begin::Slider button-->
             <button class="btn btn-icon btn-active-color-primary" id="kt_team_slider_prev1">
                 <span class="svg-icon svg-icon-3x">
-                    ...
+                    
                 </span>
             </button>
             <!--end::Slider button-->
@@ -53,7 +73,7 @@
             <!--begin::Slider button-->
             <button class="btn btn-icon btn-active-color-primary" id="kt_team_slider_next1">
                 <span class="svg-icon svg-icon-3x">
-                    ...
+                    
                 </span>
             </button>
             <!--end::Slider button-->
