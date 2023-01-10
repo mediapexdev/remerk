8db47786281bb39e4cd68a3c2c506291dd152c9d
Djielani Bodian <djielanibodian99@gmail.com>
2023-01-10 12:47:40 +0000

    init map

--- resources/views/transporteur/dashboard.blade.php
+++ resources/views/transporteur/dashboard.blade.php
@@ -41,7 +41,7 @@ $expeditions  = Expedition::where('transporteur_id', $transporteur->id)->get();
         <!--begin::Right Col--> --}}
         <div class="col-xl-8">
             <div class="col mb-5">
-                @include('transporteur.components.widgets.chart')
+                @include('transporteur.components.widgets.map')
             </div>
             <div class="col">
                 @include('transporteur.components.widgets.overview')
@@ -49,10 +49,9 @@ $expeditions  = Expedition::where('transporteur_id', $transporteur->id)->get();
         </div>
         {{--
         <!--begin::Right Col--> --}}
-    {{--
+        {{--
         <!--end::Row--> --}}
     </div>
-
 </div>
 @endsection
 
@@ -67,4 +66,135 @@ $expeditions  = Expedition::where('transporteur_id', $transporteur->id)->get();
 <script type="text/javascript" src="{{URL::asset('assets/js/custom/apps/expeditions/carrier/postulat.js')}}"></script>
 <script type="text/javascript" src="{{URL::asset('assets/js/custom/utilities/modals/create-camion.js')}}"></script>
 <script type="text/javascript" src="{{URL::asset('assets/js/custom/apps/chart/performance_transporteur.js')}}"></script>
+{{-- <script type="text/javascript" src="{{URL::asset('assets/js/custom/apps/chart/map.js')}}"></script> --}}
+
+<script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/map.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/geodata/data/countries2.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/geodata/data/countries2.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/geodata/senegalHigh.js"></script>
+<script>
+    // var continents = {
+//     "AF": 0
+//   }
+
+  // Create root element
+  // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+    var root   = am5.Root.new("chartdiv");
+    var colors = am5.ColorSet.new(root, {
+
+    });
+    
+    
+    // Set themes
+    // https://www.amcharts.com/docs/v5/concepts/themes/
+    root.setThemes([
+        am5themes_Animated.new(root)
+    ]);
+    
+    
+    // Create the map chart
+    // https://www.amcharts.com/docs/v5/charts/map-chart/
+    var chart = root.container.children.push(am5map.MapChart.new(root, {
+        panX: "rotateX",
+        projection: am5map.geoMercator()
+    }));
+    
+    var mode = KTThemeMode.getMode();
+    var color     = ('dark' === mode) ? '#f9b233' : '#068c94';
+
+  // Create polygon series for the world map
+  // https://www.amcharts.com/docs/v5/charts/map-chart/map-polygon-series/
+    var senegalMap = chart.series.push(am5map.MapPolygonSeries.new(root, {
+        geoJSON: am5geodata_senegalHigh,
+    }));
+    senegalMap.mapPolygons.template.setAll({
+        tooltipText: "{name}"+({{$expeditions->count()}}),
+        toggleKey: "active",
+        interactive: true,
+        fill: am5.color(0xaaaaaa),
+        templateField: "polygonSettings"
+    });
+    
+    senegalMap.mapPolygons.template.states.create("hover", {
+        fill: color,
+        toggleKey: "active",
+    });
+    
+
+    senegalMap.mapPolygons.template.events.on("click", (ev) => {
+
+        colorIndex = parseInt(Math.random()*30)+1;
+        senegalMap.mapPolygons.template.states.create("active", {
+        fill: colors.getIndex(colorIndex)
+        });
+
+        var dataItem = ev.target.dataItem;
+        var data     = dataItem.dataContext;
+
+        var data_    = dataItem;
+        var polygon  = dataItem.get("Polygon");
+        polygon.fill   = colors.getIndex(colorIndex);
+        console.log(polygon);
+        // data_.fill   = colors.getIndex(colorIndex);
+        //alert(polygon);
+        dataItem.fill= colors.getIndex(1);
+        //console.log(dataItem);
+        
+        var zoomAnimation = senegalMap.zoomToDataItem(dataItem);
+    
+        Promise.all([
+        zoomAnimation.waitForStop(),
+        am5.net.load("https://cdn.amcharts.com/lib/5/geodata/json/senegalDepartmentsHigh.json", chart)
+        ]).then((results) => {
+        var geodata = am5.JSONParser.parse(results[1].response);
+        countrySeries.setAll({
+            geoJSON: geodata,
+            fill: data.polygonSettings.fill,
+            //toggleKey: "active",
+        });
+
+    });
+});
+
+  // Create polygon series for the country map
+  // https://www.amcharts.com/docs/v5/charts/map-chart/map-polygon-series/
+    var countrySeries = chart.series.push(am5map.MapPolygonSeries.new(root, {
+        visible: false
+    }));
+    
+    countrySeries.mapPolygons.template.setAll({
+        tooltipText: "{name}",
+        interactive: true,
+        toggleKey: "active",
+        fill: am5.color(0xaaaaaa)
+    });
+    
+    // countrySeries.mapPolygons.template.states.create("hover", {
+    //     fill: colors.getIndex(9)
+    // });
+
+  // Set up data for countries
+//   var data = [];
+//   for(var id in am5geodata_data_countries2) {
+//     if (am5geodata_data_countries2.hasOwnProperty(id)) {
+//       var country = am5geodata_data_countries2[id];
+//       if (country.maps.length) {
+//         data.push({
+//           id: id,
+//           map: country.maps[0],
+//           polygonSettings: {
+//             fill: colors.getIndex(continents[country.continent_code]),
+//           }
+//         });
+//       }
+//     }
+//   }
+//   senegalMap.data.setAll(data);
+
+chart.set("zoomControl", am5map.ZoomControl.new(root, {}));
+
+</script>
+
 @endsection
